14378671db5fe988c218fd7bc281a5dfc5261e13
Jeremy Chia <jeremychiajianzhi@gmail.com>
2020-10-21 13:48:13 +1100

    edited variable relationships

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,4 +1,8 @@
-﻿CREATE TABLE "departments" (
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
     "dept_no" VARCHAR(10)   NOT NULL,
     "dept_name" VARCHAR(100)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
